ee5f096e87d8c64cbd4d4a4c2ee4ed315960b344
Fernando Mendes <fernando.msantos1@gmail.com>
2023-06-20 01:39:11 -0300

    add tests with rspec

--- spec/madruga_spec.rb
+++ spec/madruga_spec.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 RSpec.describe Madruga do
+  let(:file) { "spec/fixtures/files/test.txt" }
+  let(:beginning_column) { 2 }
+
   it "has a version number" do
     expect(Madruga::VERSION).not_to be nil
   end
 
-  it "has modify"
+  it "has the first line been modified" do
+    old_first_line = File.open(file, &:readline)
+    Madruga::FormatFile.perform(file, beginning_column)
+    new_first_line = File.open(file, &:readline)
+    diff_amount = old_first_line.size - new_first_line.size
+
+    expect(diff_amount).to eq(beginning_column)
+  end
 end
